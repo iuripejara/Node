4db125d671cc0a425f8a62899d587fb7f07ae480
Iuri Pejara <iuripp91@gmail.com>
2024-11-05 16:10:21 +0000

    05/11 16:10
    melhorias

--- exercicio-2/main.js
+++ exercicio-2/main.js
@@ -1,10 +1,17 @@
 const path  = require ("node:path")
 const os    = require ("node:os")
 const fs    = require ("node:fs")
-function Sistemasops() {
-    const sistema = os.type()
+
+const systemaMap ={
+    "win32" : "Windows",
+    "linux" : "Linux",
+    "darwin" : "MacOS"
+}
+
+function ativarFuncao (){
+    const sistema = systemaMap[os.platform()]
     const arquitetura = os.arch()
-    const procesadore = os.cpus()
+    const procesadore = os.cpus()[0].model
     let tempoativo = os.uptime
     
     let tempoMinuto = tempoativo    /60
@@ -18,15 +25,40 @@ function Sistemasops() {
     tempoMinuto =   tempoMinuto %60
     tempoHora   =   tempoHora   %60
 
-    console.log(`tempo Ativo: ${tempoHora} hrs ${tempoMinuto} minu ${tempoativo} segu`)
+    
 
 
     const memoriaTotal      = os.totalmem()
     const memoriasLivre     = os.freemem()
     const memoriaEmUso      = memoriaTotal - memoriasLivre
 
-    console.log(`Uso da memoria ${((memoriaEmUso / memoriaTotal) * 100).toFixed(2)}%`)
-
-    const newFile = fs.writeFile(path())
+    return {sistema, arquitetura, procesadore,tempoHora,tempoMinuto,tempoativo,memoriaEmUso}
 }
-Sistemasops()
+
+function Sistemasops({sistema, arquitetura, procesadore,tempoHora,tempoMinuto,tempoativo,memoriaEmUso,memoriaTotal}) {
+   
+    console.clear()
+    console.log(`Sistesma Operacional ${sistema} Arquitetura ${arquitetura}`)
+    console.log(`Procesador ${procesadore}`)
+    console.log(`Uso da memoria ${((memoriaEmUso / memoriaTotal) * 100).toFixed(2)}%`)
+    console.log(`tempo Ativo: ${tempoHora} hrs ${tempoMinuto} minu ${tempoativo} segu`)
+}
+
+function savelog ({sistema, arquitetura, procesadore,tempoHora,tempoMinuto,tempoativo,memoriaEmUso,memoriaTotal}){
+    const log = `Sistesma Operacional ${sistema} Arquitetura ${arquitetura} | Procesador ${procesadore} | 
+    Uso da memoria ${((memoriaEmUso / memoriaTotal) * 100).toFixed(2)}% | tempo Ativo: ${tempoHora} hrs ${tempoMinuto} minu ${tempoativo} segu)
+    `
+    const logDir = path.join ("/", "log")
+    if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir)
+    }
+
+    const logPath = path.join(logDir,"log.txt")
+    fs.appendFileSync(logPath, log)
+}
+
+setInterval(()=>{
+    const systemInfo = ativarFuncao()
+    Sistemasops(systemInfo)
+    savelog(systemInfo)
+},1000)
